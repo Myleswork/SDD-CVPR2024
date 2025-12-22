3563c31948d2fdb1895a6f9c6e9a63bd845adca6
mylesz <mingsheng1712@163.com>
2025-12-22 10:27:17 +0800

    [bash_change]

--- teacher_unpair.sh
+++ teacher_unpair.sh
@@ -1,32 +1,32 @@
 # res50_shuv1
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/res50_shuv1.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/res50_shuv1.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/res50_shuv1.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/res50_shuv1.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/res50_shuv1.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/res50_shuv1.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/res50_shuv1.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/res50_shuv1.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/res50_shuv1.yaml --M [1,2]
 
 # res32x4_mv2
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/res32x4_mv2.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/res32x4_mv2.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/res32x4_mv2.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/res32x4_mv2.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/res32x4_mv2.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/res32x4_mv2.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/res32x4_mv2.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/res32x4_mv2.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/res32x4_mv2.yaml --M [1,2]
 
 # wrn40_2_vgg8
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/wrn40_2_vgg8.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/wrn40_2_vgg8.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/wrn40_2_vgg8.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/wrn40_2_vgg8.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/wrn40_2_vgg8.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/wrn40_2_vgg8.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/wrn40_2_vgg8.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/wrn40_2_vgg8.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/wrn40_2_vgg8.yaml --M [1,2]
 
 # wrn40_2_mv2
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/wrn40_2_mv2.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/wrn40_2_mv2.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_DKD/wrn40_2_mv2.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/wrn40_2_mv2.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/wrn40_2_mv2.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_KD/wrn40_2_mv2.yaml --M [1,2]
-python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/wrn40_2_mv2.yaml --M [1,2,4]
+# python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/wrn40_2_mv2.yaml --M [1,2,4]
 python train_origin.py --cfg configs/cifar100/AFPN_SDD_NKD/wrn40_2_mv2.yaml --M [1,2]
 
